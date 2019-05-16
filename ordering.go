a4d1151eb53dc05b64b8b47e0da78d966be6252c
ninedraft <clamores.pro@gmail.com>
2019-05-16 15:38:53 +0300

    adds source ordering engine

---
+++ ordering.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"strings"
+)
+
+type SourceOrdering struct {
+	indexes  map[string]int
+	topIndex int
+}
+
+func NewSourceOrdering(sources []string) *SourceOrdering {
+	var indexes = make(map[string]int, len(sources))
+	for _, source := range sources {
+		indexes[source] = 0
+	}
+	return &SourceOrdering{
+		indexes:  indexes,
+		topIndex: 0,
+	}
+}
+
+func (ordering *SourceOrdering) IndexSource(source string) int {
+	return ordering.indexes[source]
+}
+
+func (ordering *SourceOrdering) TopIndex() int {
+	return ordering.topIndex
+}
+
+func (ordering *SourceOrdering) ScanText(text string) {
+	for source, i := range ordering.indexes {
+		var index = strings.Index(text, "["+source+"]")
+		if index < 0 {
+			continue
+		}
+		if i == 0 {
+			ordering.topIndex++
+			ordering.indexes[source] = ordering.topIndex
+		}
+	}
+}
+
+func (ordering *SourceOrdering) Index() map[string]int {
+	var cp = make(map[string]int, len(ordering.indexes))
+	for source, index := range ordering.indexes {
+		if index > 0 {
+			cp[source] = index
+		}
+	}
+	return cp
+}
+
+func (ordering *SourceOrdering) OrderedSources() []string {
+	var orderedSources = make([]string, len(ordering.indexes))
+	for source, index := range ordering.indexes {
+		if index > 0 {
+			orderedSources[index-1] = source
+		}
+	}
+	return orderedSources
+}
